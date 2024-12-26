de23babcecbd0bf70c95c1d286899d76acb8df68
0tae <choiyt3465@naver.com>
2024-12-26 16:24:11 +0900

    init

---
+++ src/test/java/lineword/vacation_backend/VacationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lineword.vacation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
