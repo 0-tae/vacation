1c7204bceb14f24758f4c41e4852d9d9b863ea4a
0tae <choiyt3465@naver.com>
2025-01-02 08:16:26 +0900

    feat: google oauth 도메인 구현

--- src/main/java/lineword/vacation_backend/domain/GoogleOauthAccount.java
+++ src/main/java/lineword/vacation_backend/domain/GoogleOauthAccount.java
@@ -3,16 +3,34 @@ package lineword.vacation_backend.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+
 @Entity
-@Table(name = "Role")
-public class Role {
+@Table(name = "GoogleOauthAccount")
+public class GoogleOauthAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 16)
-    private String name;
+    @Column(name = "account_id", nullable = false, length = 32)
+    private String accountId;
+
+    @Column(name = "access_token", nullable = false, length = 64)
+    private String accessToken;
+
+    @Column(name = "refresh_token", nullable = false, length = 64)
+    private String refreshToken;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDate createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDate deletedAt;
+
+    @OneToOne
+    @JoinColumn(name = "oauth_account_id", nullable = false)
+    private MemberOauthAccount memberOauthAccount;
 
     // Getters and Setters
 }
+
